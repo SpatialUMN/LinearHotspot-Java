8d7b6cf57fb356a55dda8388c355c14a82a5c06d
SpatialUMN <47234586+SpatialUMN@users.noreply.github.com>
2019-05-06 17:05:41 -0700

    Update GetHotspots.java

--- src/GetHotspots.java
+++ src/GetHotspots.java
@@ -44,7 +44,7 @@ public static void main(String[] args) throws IOException {
 		NetworkDataset.nodeIDList.clear();
 		NetworkDataset.numTotalNodes = 0;
 		NetworkDataset.nodeAdjacencyMap.clear();
-		shortestPathTreeXun.
+		
 		
 	}
 	Arrays.sort(ActivityPairMC.topdensity);
